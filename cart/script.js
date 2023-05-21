3e9f99933d3292f991a3657eb10e5c6eb309932c
shunsuke-tamura <wwwxx4869@gmail.com>
2023-05-21 10:01:41 +0900

    update: いろいろ繋ぎこみ

---
+++ cart/script.js
@@ -0,0 +1,39 @@
+import products from "../static/db.js";
+
+const cartItemIds = localStorage.getItem("cartItemIds");
+const cartItemIdList = cartItemIds.split(",");
+
+const cartItems = document.getElementById("cart-items");
+
+for (const id of cartItemIdList) {
+  const productIndex = products.findIndex((e) => e.id === id);
+  const product = products[productIndex];
+
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("grid-item");
+
+  const img = document.createElement("img");
+  img.classList.add("product-image");
+  img.src = `../static/${product.img}`;
+
+  const h3 = document.createElement("h3");
+  h3.textContent = product.title;
+
+  const price = document.createElement("p");
+  price.classList.add("product-price");
+  price.textContent = `¥${product.price}`;
+
+  const button1 = document.createElement("button");
+  button1.textContent = "後で買う";
+
+  const button2 = document.createElement("button");
+  button2.textContent = "削除";
+
+  cartItem.appendChild(img);
+  cartItem.appendChild(h3);
+  cartItem.appendChild(price);
+  cartItem.appendChild(button1);
+  cartItem.appendChild(button2);
+
+  cartItems.appendChild(cartItem);
+}
---
+++ product_details/script.js
@@ -0,0 +1,42 @@
+import products from "../static/db.js";
+
+const url = new URL(location.href);
+const params = new URLSearchParams(url.search);
+const id = params.get("id");
+const productIndex = products.findIndex((e) => e.id === id);
+const product = products[productIndex];
+
+const titleElent = document.getElementsByClassName("detail-title")[0];
+titleElent.textContent = `${product.title}`;
+
+const detailimg = document.getElementsByClassName("detail-img")[0];
+detailimg.src = `../static/${product.img}`;
+
+const p = document.getElementsByClassName("star")[0];
+const span = document.getElementsByClassName("star5_rating")[0];
+const t = document.createTextNode(`(${product.reviewNum}件)`);
+p.textContent = "";
+span.setAttribute("data-rate", `${product.review}`);
+p.appendChild(span);
+p.appendChild(t);
+
+const price = document.getElementsByClassName("detail-price")[0];
+price.textContent = `¥${product.price}`;
+
+const company = document.getElementsByClassName("company")[0];
+company.textContent = `${product.from}`;
+
+const addCartButton = document.getElementById("add-cart-button");
+
+function addCart() {
+  console.log("click");
+  const cartItemIds = localStorage.getItem("cartItemIds");
+  console.log(cartItemIds);
+  if (cartItemIds != null) {
+    localStorage.setItem(`cartItemIds`, `${cartItemIds},${id}`);
+  } else {
+    localStorage.setItem(`cartItemIds`, `${id}`);
+  }
+}
+
+addCartButton.addEventListener("click", addCart);
