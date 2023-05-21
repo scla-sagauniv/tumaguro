971915d357f4b0777be73b5d27f721df0fd82617
shunsuke-tamura <wwwxx4869@gmail.com>
2023-05-21 15:19:27 +0900

    update: 罠追加

--- buy/script.js
+++ buy/script.js
@@ -24,3 +24,10 @@ function submitA() {
 
 const submitButton = document.getElementsByClassName("buy")[0];
 submitButton.addEventListener("click", submitA);
+
+const danger = document.getElementById("danger");
+danger.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    event.target.parentNode.remove();
+  }
+});
